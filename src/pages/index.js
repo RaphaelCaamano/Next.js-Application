cc667d0b2d2b58c102180a9bd02921db8f28c9fe
Raphael Caamano <155185862+RaphaelCaamano@users.noreply.github.com>
2025-05-17 21:08:31 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ export default function Home() {
       >
         <main className={styles.main}>
           
-          <h1>Raphael's amazing Next app + Git!</h1>
+          <h1>Raphael's Amazing Next App + Git!</h1>
 
           <div className={styles.ctas}>
             <a
